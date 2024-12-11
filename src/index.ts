e1c0451a9775ea58203fe1d89b2608b28f63e883
neverland <chenjiahan.jait@bytedance.com>
2024-12-11 17:32:15 +0800

    feat: basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import type { RsbuildPlugin } from '@rsbuild/core';
+import { logger, type RsbuildPlugin } from '@rsbuild/core';
+import { publint, type Options } from 'publint';
+import { formatMessage } from 'publint/utils';
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
 
 export type PluginPublintOptions = {
-  foo?: string;
-  bar?: boolean;
+  /**
+   * Options for publint.
+   * @see https://github.com/bluwy/publint/blob/master/pkg/README.md#api
+   */
+  publintOptions?: Options;
 };
 
 export const pluginPublint = (
@@ -10,7 +17,32 @@ export const pluginPublint = (
 ): RsbuildPlugin => ({
   name: 'plugin-publint',
 
-  setup() {
-    console.log('Hello Rsbuild!', options);
+  setup(api) {
+    api.onAfterBuild(async () => {
+      const mergedOptions = {
+        pkgDir: api.context.rootPath,
+        ...options.publintOptions,
+      };
+      const { messages } = await publint(mergedOptions);
+
+      const pkg = JSON.parse(
+        await fs.readFile(join(mergedOptions.pkgDir, 'package.json'), 'utf8'),
+      );
+
+      let hasError = false;
+
+      for (const message of messages) {
+        if (message.type === 'error') {
+          logger.error(`[publint] ${formatMessage(message, pkg)}`);
+          hasError = true;
+        } else {
+          logger.warn(`[publint] ${formatMessage(message, pkg)}`);
+        }
+      }
+
+      if (hasError) {
+        process.exit(1);
+      }
+    });
   },
 });
